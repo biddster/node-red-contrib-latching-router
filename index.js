86d106749eacba612cc079366be666957105e0ab
biddster <luke.biddell@gmail.com>
2016-11-07 15:25:00 +0000

    First pass of basic function working.

---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ The MIT License (MIT)
+
+ Copyright (c) 2016 @biddster
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+module.exports = function (RED) {
+    'use strict';
+
+    var _ = require('lodash');
+
+    RED.nodes.registerType('latching-router', function (config) {
+
+        RED.nodes.createNode(this, config);
+        var node = this;
+        if (!node.context().get('latchOutput')) {
+            node.context().set('latchOutput', 1);
+        }
+
+        node.on('input', function (msg) {
+            if (_.has(msg, 'payload.latchOutput')) {
+                useLatchOutput(Number(msg.payload.latchOutput));
+            } else if (_.isString(msg.payload) && msg.payload.indexOf('latchOutput') >= 0) {
+                var newOutput = msg.payload.split(':')[1].trim();
+                useLatchOutput(Number(newOutput));
+            } else {
+                var msgs = new Array(config.outputs);
+                msgs[node.context().get('latchOutput') - 1] = msg;
+                node.send(msgs);
+            }
+        });
+
+        function useLatchOutput(newOutput) {
+            if (newOutput > config.outputs || newOutput < 1) {
+                node.status({
+                    fill: 'red',
+                    shape: 'dot',
+                    text: 'Invalid output ' + newOutput
+                });
+            } else {
+                node.context().set('latchOutput', newOutput);
+                indicateLatchedOutput();
+            }
+        }
+
+        function indicateLatchedOutput() {
+            node.status({
+                fill: 'green',
+                shape: 'dot',
+                text: 'Output => ' + node.context().get('latchOutput')
+            });
+        }
+
+        indicateLatchedOutput();
+    });
+};
---
+++ tests/test.js
@@ -0,0 +1,95 @@
+/**
+ The MIT License (MIT)
+
+ Copyright (c) 2016 @biddster
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+"use strict";
+var assert = require('assert');
+var nodeRedModule = require('../index.js');
+
+function loadNode(config, redModule) {
+    var _events = [], _status = undefined, _error = undefined, _messages = [];
+    var RED = {
+        nodes: {
+            registerType: function (nodeName, nodeConfigFunc) {
+                this.nodeConfigFunc = nodeConfigFunc;
+            },
+            createNode: function () {
+            }
+        },
+        on: function (event, eventFunc) {
+            _events[event] = eventFunc;
+        },
+        emit: function (event, data) {
+            _events[event](data);
+        },
+        error: function (error) {
+            if (error) _error = error;
+            return _error;
+        },
+        status: function (status) {
+            if (status) _status = status;
+            return _status;
+        },
+        log: function () {
+            console.log.apply(this, arguments);
+        },
+        send: function (msg) {
+            assert(msg);
+            _messages.push(msg);
+        },
+        messages: function (messages) {
+            if (messages) _messages = messages;
+            return _messages;
+        }
+    };
+    redModule(RED);
+    RED.nodes.nodeConfigFunc.call(RED, config);
+    return RED;
+}
+
+describe('latching-router', function () {
+
+    it('should work ', function () {
+        var node = loadNode({
+            outputs: 3
+        }, nodeRedModule);
+
+        node.emit('input', {payload: {_latchOutput: 1}});
+        var msg0 = {payload: '0'};
+        node.emit('input', msg0);
+        assert.strictEqual(1, node.messages().length);
+        assert.strictEqual(msg0, node.messages()[0][0]);
+
+        node.emit('input', {payload: {_latchOutput: 2}});
+        var msg1 = {payload: '1'};
+        node.emit('input', msg1);
+        assert.strictEqual(2, node.messages().length);
+        assert.strictEqual(msg1, node.messages()[1][1]);
+
+        var msg2 = {payload: '1'};
+        node.emit('input', msg2);
+        assert.strictEqual(3, node.messages().length);
+        assert.strictEqual(msg2, node.messages()[2][1]);
+    });
+
+});
